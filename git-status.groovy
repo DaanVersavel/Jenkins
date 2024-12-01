b8e479de3a1977d49918fa1b472c619a23f5be06
Daan Versavel <92854110+DaanVersavel@users.noreply.github.com>
2024-12-01 17:43:56 +0100

    Update git-status.groovy

--- git-status.groovy
+++ git-status.groovy
@@ -20,7 +20,7 @@ pipeline {
         stage("Get source code ") {
             steps{
                 script{
-                    echo "Cloning the repository from ${repo} on branch ${branch}..."
+                    echo "Cloning the repository from ${repo} on branch ${branch}.."
                     git branch: "${branch}", url: "${repo}"
                     echo "succesfull"
                 }
